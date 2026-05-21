edbeb1f0135a4f00a1b54e7b73feb03f85dbfb76
bush-ai <bushchallengersintel@gmail.com>
2026-05-20 22:17:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/report1/",
+  base: "/report-major3/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
